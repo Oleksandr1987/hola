cbcf9608480eaf9fceba87b811d915c2235a193d
Oleksansr1987 <solovij1987@gmail.com>
2021-11-09 17:26:26 +0200

    add following functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,14 +66,14 @@ before_action :set_user, only: %i[show edit update destroy following followers]
   def following
     @title = "Following"
     @user  = User.find(params[:id])
-    # @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
-    # @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
 
